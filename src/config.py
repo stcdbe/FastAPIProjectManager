d4f35536df6c05af093075f3deadcbbd5188d8d0
stcdbe <samthomascolddeadblueeyes@gmail.com>
2025-10-17 13:19:23 +1000

    Update env vars, add PG_URL and PG_URL_TEST

--- src/config.py
+++ src/config.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 
-from pydantic import DirectoryPath, EmailStr
+from pydantic import DirectoryPath, EmailStr, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="./.env",
+        case_sensitive=True,
+    )
+
     DEBUG: bool
     PORT: int
     DOCS_URL: str | None = None
@@ -16,19 +21,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRES: int
     REFRESH_TOKEN_EXPIRES: int
 
-    _PG_URL: str = "postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+    PG_URL: PostgresDsn
 
-    PG_USER: str
-    PG_PASSWORD: str
-    PG_HOST: str
-    PG_PORT: str
-    PG_DB: str
-
-    PG_USER_TEST: str
-    PG_PASSWORD_TEST: str
-    PG_HOST_TEST: str
-    PG_PORT_TEST: str
-    PG_DB_TEST: str
+    PG_URL_TEST: PostgresDsn
 
     EMAIL_SMTP_SERVER: str
     EMAIL_PORT: int
@@ -36,28 +31,6 @@ class Settings(BaseSettings):
     EMAIL_PASSWORD: str
     EMAIL_SENDER: EmailStr
     TEST_EMAIL_RECEIVER: EmailStr
-
-    @property
-    def PG_URL(self) -> str:
-        return self._PG_URL.format(
-            PG_USER=self.PG_USER,
-            PG_PASSWORD=self.PG_PASSWORD,
-            PG_HOST=self.PG_HOST,
-            PG_PORT=self.PG_PORT,
-            PG_DB=self.PG_DB,
-        )
-
-    @property
-    def PG_URL_TEST(self) -> str:
-        return self._PG_URL.format(
-            PG_USER=self.PG_USER_TEST,
-            PG_PASSWORD=self.PG_PASSWORD_TEST,
-            PG_HOST=self.PG_HOST_TEST,
-            PG_PORT=self.PG_PORT_TEST,
-            PG_DB=self.PG_DB_TEST,
-        )
-
-    model_config = SettingsConfigDict(env_file="./.env", case_sensitive=True)
 
 
 settings = Settings()
