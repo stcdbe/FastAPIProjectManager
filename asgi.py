930772865ae391ec2eaa64c23d5487389c453b67
stcdbe <134695435+stcdbe@users.noreply.github.com>
2026-01-14 20:45:44 +1000

    Update asgi.py

--- asgi.py
+++ asgi.py
@@ -1,4 +1,4 @@
-from logging.config import dictConfig
+from logging.config import dictConfig as loggingDictConfig
 
 import uvicorn
 
@@ -8,7 +8,7 @@ from src.main import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    dictConfig(get_settings().LOG_CONFIG)
+    loggingDictConfig(get_settings().LOG_CONFIG)
     uvicorn.run(
         app="asgi:app",
         host=get_settings().HOST,
