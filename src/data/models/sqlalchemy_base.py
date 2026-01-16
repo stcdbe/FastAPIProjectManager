72ba5adedc218b0aca4e4cdd09c365ffcf1879de
stcdbe <134695435+stcdbe@users.noreply.github.com>
2026-01-16 15:03:35 +1000

    Update sqlalchemy_base.py

--- src/data/models/sqlalchemy_base.py
+++ src/data/models/sqlalchemy_base.py
@@ -4,15 +4,18 @@ from uuid import UUID
 from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.util import immutabledict
 
-_NAMING_CONVENTION: Final[dict[str, str]] = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}
 
+_NAMING_CONVENTION: Final[immutabledict[str, str]] = immutabledict(
+    {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    },
+)
 
 class SQLAlchemyBaseModel(DeclarativeBase, AsyncAttrs):
     metadata = MetaData(naming_convention=_NAMING_CONVENTION)
@@ -21,7 +24,7 @@ class SQLAlchemyBaseModel(DeclarativeBase, AsyncAttrs):
     guid: Mapped[UUID] = mapped_column(primary_key=True)
 
     def __str__(self) -> str:
-        return f"SQL Record (Table: {self.__tablename__!r} GUID: {self.guid})"
+        return f"SQL Record <Table: {self.__tablename__!r}, GUID: {self.guid!r}>"
 
     def __repr__(self) -> str:
         return self.__str__()
